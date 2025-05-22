2b5ca0c6f16937c212aef1e501edf8ca5f487fed
Stephane Vogrig <svogrig@1E6.42angouleme.fr>
2025-05-22 03:22:38 +0200

    ex02: preparation insertion: creation main et pend

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 02:04:00 by svogrig           #+#    #+#             */
-/*   Updated: 2025/05/22 00:16:34 by svogrig          ###   ########.fr       */
+/*   Updated: 2025/05/22 03:20:24 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,6 +69,41 @@ void displayLevel(size_t elementSize)
 	displaySubtest(std::string(oss.str()), FG_PURPLE);
 }
 
+void insertion(t_vector & vector, size_t elementSize)
+{
+	displayLevel(elementSize);
+	displayVectorByPair(FG_PURPLE "input  :", vector, elementSize);
+	size_t nbr_element = vector.size() / elementSize;
+	size_t main_nbr_element = 1 + nbr_element / 2;
+	size_t pend_nbr_element = nbr_element - main_nbr_element;
+	std::cout	<< "nbr_element: " << nbr_element << " main_nbr_element; " << main_nbr_element
+				<< " pend_nbr_element: " << pend_nbr_element << std::endl;
+
+	t_vector main(main_nbr_element * elementSize, 0);
+	t_vector::iterator it_last = std::copy(vector.begin(), vector.begin() + elementSize, main.begin()); // b1
+	(void)it_last;
+	for (size_t i = 1; i < nbr_element; i += 2) // a1, a2, ...
+	{
+		// std::cout << " i:" << i << std::flush;
+		t_vector::iterator start = vector.begin() + i * elementSize;
+		it_last = std::copy(start, start + elementSize, it_last);
+	}
+	// std::cout << std::endl;
+	displayVectorByPair(FG_PURPLE "main   :", main, elementSize);
+
+	t_vector pend(pend_nbr_element * elementSize, 0);
+	it_last = pend.begin();
+	for (size_t i = 2; i < nbr_element; i += 2 ) // b2, b3, ...
+	{
+		// std::cout << "i:" << i << std::flush;
+
+		t_vector::iterator start = vector.begin() + i * elementSize;
+		it_last = std::copy(start, start + elementSize, it_last);
+	}
+	// std::cout << std::endl;
+	displayVectorByPair(FG_PURPLE "pend   :", pend, elementSize);
+}
+
 void swapVector(t_vector::iterator ita, t_vector::iterator itb, int elementSize)
 {
 	for (int i = 0; i < elementSize; ++i)
@@ -94,16 +129,9 @@ void PmergeMe::sort(t_vector & vector, size_t elementSize)
 	displayVectorByPair(FG_PURPLE "sorted :", vector, elementSize);
 	
 	sort(vector, elementSize * 2);
-	// size_t nbrPair = nbrElement / 2;
-	// if (nbrElement < 3)
-	// 	return ;
-	// t_vector main;
-	// // copy b1 in main
-	
-	// t_vector pend;
-	
-	
-	
+	if (nbrElement < 3)
+		return ;
+	insertion(vector, elementSize);
 }
 
 void PmergeMe::mergeInsertSort(t_vector & vector)
